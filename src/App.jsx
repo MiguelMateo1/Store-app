b3efbfc25cc33bf9d959a490c74d28611458c5d3
miguel mateo <mmateo95@hotmail.com>
2023-06-21 20:49:12 -0400

    gave body tag an id/ added background color change on click func

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,28 @@ import Nav from './components/Sidebars/Nav'
 import ProductInfo from './components/ProductInfo'
 import ProductSelect from './components/ProductSelect'
 import { DataProvider } from './context/DataContext'
+import { useEffect } from 'react'
 
 function App() {
+  // backgourn colors
+  const bgColors = ['#e7be34','#555552','#a4bbb5', '#e0ded7'];
+  let num = 0;
+  
+  // changes bg color when click on backgound
+  const handleClickColor = (e) => {
+    if (num == bgColors.length) {
+      num = 0;
+    };
+
+    if (e.srcElement.id == 'bg') {
+    document.body.style.backgroundColor = bgColors[num];
+    num += 1;
+    };
+  }
+
+  useEffect(() => {
+    document.body.addEventListener('click', handleClickColor)
+  }, []);
 
   return (
     <main>
