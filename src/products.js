f97f1b263a90df2cd0a6b5f815723974d9d69712
miguel mateo <mmateo95@hotmail.com>
2023-06-23 11:53:51 -0400

    updated list and change price to num instead of string

--- src/products.js
+++ src/products.js
@@ -1,15 +1,15 @@
 export const shoes = [{
         key: 1,
-        name: 'shoe1',
-        price: '$65',
-        color: '#dd5050',
-        img: '/image/shoes/shoe11.png',
-        imgMain: '../public/image/shoes/shoe1.png',
-        description: 'information and details of your product on your ecommerce site.'
+        name: 'shoe3',
+        price: 70,
+        color: '#718E84',
+        img: '/image/shoes/shoe77.png',
+        imgMain: '/image/shoes/shoe7.png',
+        description: 'information and details of your product on your ecommerce site.',
     },{
         key: 2,
         name: 'shoe2',
-        price: '$85',
+        price: 85,
         color: '#B6A086',
         img: '/image/shoes/shoe22.png',
         imgMain: '../public/image/shoes/shoe2.png',
@@ -17,31 +17,33 @@ export const shoes = [{
     },{
         key: 3,
         name: 'shoe3',
-        price: '$80',
+        price: 80,
         color: '#F4C679',
         img: '/image/shoes/shoe33.png',
         imgMain: '/image/shoes/shoe3.png',
         description: 'information and details of your product on your ecommerce site.'
     },{
         key: 4,
-        name: 'shoe3',
-        price: '$80',
-        color: '#E88088',
-        img: '/image/shoes/shoe44.png',
-        imgMain: '/image/shoes/shoe4.png',
-        description: 'information and details of your product on your ecommerce site.'
+        name: 'shoe1',
+        price: 65,
+        color: '#dd5050',
+        img: '/image/shoes/shoe11.png',
+        imgMain: '../public/image/shoes/shoe1.png',
+        description: 'information and details of your product on your ecommerce site.',
+        category: 'new'
     },{
         key: 5,
         name: 'shoe3',
-        price: '$80',
+        price: 90,
         color: '#6CA8BB',
         img: '/image/shoes/shoe55.png',
         imgMain: '/image/shoes/shoe5.png',
-        description: 'information and details of your product on your ecommerce site.'
+        description: 'information and details of your product on your ecommerce site.',
+        category: 'new'
     },{
         key: 6,
         name: 'shoe3',
-        price: '$80',
+        price: 68,
         color: '#227FD6',
         img: '/image/shoes/shoe66.png',
         imgMain: '/image/shoes/shoe6.png',
@@ -49,16 +51,16 @@ export const shoes = [{
     },{
         key: 7,
         name: 'shoe3',
-        price: '$70',
-        color: '#718E84',
-        img: '/image/shoes/shoe77.png',
-        imgMain: '/image/shoes/shoe7.png',
+        price: 80,
+        color: '#E88088',
+        img: '/image/shoes/shoe44.png',
+        imgMain: '/image/shoes/shoe4.png',
         description: 'information and details of your product on your ecommerce site.'
     },{
         key: 8,
         name: 'shoe3',
-        price: '$75',
-        color: '#B8B8B8',
+        price: 75,
+        color: '#a8a8a8',
         img: '/image/shoes/shoe88.png',
         imgMain: '/image/shoes/shoe8.png',
         description: 'information and details of your product on your ecommerce site.'
