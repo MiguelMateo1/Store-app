2be7abc304d0244c99ed4ff98193e60c1a1c58fc
miguel mateo <mmateo95@hotmail.com>
2023-06-24 17:27:47 -0400

    img added

--- src/About.jsx
+++ src/About.jsx
@@ -1,24 +1,30 @@
 import './App.css'
+import aboutImg from './assets/about-img.png'
 
 function About() {
 
   return (
-    <div className='about-us'>
-        <h4>about us</h4>
-        <p>
-        Welcome to our shoe company, where style meets comfort and quality!
-        We are passionate about providing footwear that not only
-        complements your unique fashion sense but also ensures a 
-        comfortable and enjoyable walking experience. Crafted with utmost 
-        care and using the finest materials, our shoes boast durability 
-        and longevity, promising to be your trusted companions in every 
-        step of your journey. We take pride
-        in our commitment to customer satisfaction, striving to exceed
-        your expectations by offering exceptional service and
-        attention to detail. Let us take you on a remarkable journey of
-        style, comfort, and confidence.
-        </p>
-    </div>
+    <>
+      <div className='about-img-container'>
+          <img src={aboutImg}></img>
+      </div>
+      <div className='about-us'>
+          <h4>about us</h4>
+          <p>
+          Welcome to our shoe company, where style meets comfort and quality!
+          We are passionate about providing footwear that not only
+          complements your unique fashion sense but also ensures a 
+          comfortable and enjoyable walking experience. Crafted with utmost 
+          care and using the finest materials, our shoes boast durability 
+          and longevity, promising to be your trusted companions in every 
+          step of your journey. We take pride
+          in our commitment to customer satisfaction, striving to exceed
+          your expectations by offering exceptional service and
+          attention to detail. Let us take you on a remarkable journey of
+          style, comfort, and confidence.
+          </p>
+      </div>
+    </>
   )
 }
 
