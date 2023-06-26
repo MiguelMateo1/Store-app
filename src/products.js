6eada5a6b4c31c28305715bd7c234e76653d534c
miguel mateo <mmateo95@hotmail.com>
2023-06-25 20:05:09 -0400

    finalized products list/ added descriptions and names

--- src/products.js
+++ src/products.js
@@ -1,105 +1,105 @@
 export const shoes = [{
         key: 1,
-        name: 'shoe3',
+        name: 'Stealth Force',
         price: 70,
         color: '#718E84',
         img: '/image/shoes/shoe77.png',
         imgMain: '/image/shoes/shoe7.png',
-        description: 'information and details of your product on your ecommerce site.',
+        description: 'Our favorite "Stealth Force" - lightweight materials and advanced cushioning technology, this sneaker ensures a responsive and comfortable stride throughout the day.',
     },{
         key: 2,
-        name: 'shoe2',
+        name: 'Eclipse',
         price: 85,
         color: '#B6A086',
         img: '/image/shoes/shoe22.png',
         imgMain: '../public/image/shoes/shoe2.png',
-        description: 'information and details of your product on your ecommerce site.'
+        description: 'The "Eclipse" - where style meets performance. Lightweight, comfortable, and responsive, these sneakers are designed to elevate your performance.',
     },{
         key: 3,
-        name: 'shoe3',
+        name: 'Hyper Volt',
         price: 80,
         color: '#F4C679',
         img: '/image/shoes/shoe33.png',
         imgMain: '/image/shoes/shoe3.png',
-        description: 'information and details of your product on your ecommerce site.'
+        description: 'This is our "Hyper Volt!" - fast and advanced technology, the Hyper Volt ensures responsive steps and enhance athletic movements.',
     },{
         key: 4,
-        name: 'shoe1',
+        name: 'Blaze Runner',
         price: 65,
         color: '#dd5050',
         img: '/image/shoes/shoe11.png',
         imgMain: '../public/image/shoes/shoe1.png',
-        description: 'information and details of your product on your ecommerce site.',
+        description: 'The "Blaze Runner" - sleek, fast, and comfortable. Lightweight design, superior support, and exceptional performance with every stride.',
         category: 'new'
     },{
         key: 5,
-        name: 'shoe3',
+        name: 'Lunar Dash',
         price: 90,
         color: '#6CA8BB',
         img: '/image/shoes/shoe55.png',
         imgMain: '/image/shoes/shoe5.png',
-        description: 'information and details of your product on your ecommerce site.',
+        description: 'The "Lunar Dash" - comfort and style in one. Experience a smooth and responsive ride for all your activities.',
         category: 'new'
     },{
         key: 6,
-        name: 'shoe3',
+        name: 'Sonic Dash',
         price: 68,
         color: '#227FD6',
         img: '/image/shoes/shoe66.png',
         imgMain: '/image/shoes/shoe6.png',
-        description: 'information and details of your product on your ecommerce site.'
+        description: 'Lightweight materials and advanced cushioning technology, the Sonic Dash is out most fast and responsive sneaker.',
     },{
         key: 7,
-        name: 'shoe3',
+        name: 'Hyper Pink',
         price: 80,
         color: '#E88088',
         img: '/image/shoes/shoe44.png',
         imgMain: '/image/shoes/shoe4.png',
-        description: 'information and details of your product on your ecommerce site.'
+        description: 'Lightweight materials and advanced cushioning technology, the Hyper Pink ensures a responsive and comfortable stride throughout the day.',
     },{
         key: 8,
-        name: 'shoe3',
+        name: 'Vapor Jet',
         price: 75,
         color: '#a8a8a8',
         img: '/image/shoes/shoe88.png',
         imgMain: '/image/shoes/shoe8.png',
-        description: 'information and details of your product on your ecommerce site.'
+        description: 'The "Vapor Jet" - sleek, fast, and comfortable. Experience lightweight design, superior support, and exceptional performance with every stride.',
     }
 ];
 
 export const accessories = [{
     key: 9,
-    name: 'as1',
+    name: 'Elegance Pearl-1',
     price: 23,
     color: '#2D7A8F',
     img: '/image/accessories/acc1.png',
     imgMain: '/image/accessories/acc1.png',
-    description: 'information and details of your product on your ecommerce site.'
+    description: 'Introducing the "Elegance Pearl" bracelet, a timeless blend of sophistication and beauty. Elevate your style effortlessly with this accessory.'
 },{
     key: 10,
-    name: 'as2',
+    name: 'Elegance Pearl-2',
     price: 25,
     color: '#8476AB',
     img: '/image/accessories/acc2.png',
     imgMain: '/image/accessories/acc2.png',
-    description: 'information and details of your product on your ecommerce site.'
+    description: 'Introducing the "Elegance Pearl" bracelet, a timeless blend of sophistication and beauty. Elevate your style effortlessly with this accessory.'
 },{
     key: 11,
-    name: 'as3',
+    name: 'Harmony Breeze-1',
     price: 25,
     color: '#FFA46F',
     img: '/image/accessories/acc3.png',
     imgMain: '/image/accessories/acc3.png',
-    description: 'information and details of your product on your ecommerce site.',
+    description: 'The "Harmony Breeze" bracelet - an elegant expression of serenity and style. Embrace tranquility and elevate your look effortlessly.',
     category: 'new'
 },{
     key: 12,
-    name: 'as2',
+    name: 'Harmony Breeze-1',
     price: 30,
     color: '#D6B1C7',
     img: '/image/accessories/acc4.png',
     imgMain: '/image/accessories/acc4.png',
-    description: 'information and details of your product on your ecommerce site.',
+    description: 'The "Harmony Breeze" bracelet - an elegant expression of serenity and style. Embrace tranquility and elevate your look effortlessly.',
     category: 'new'
 }
 ];
